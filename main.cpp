79dedf3d99883b2e1d0d0ecd54a6fdad1ac9f454
ssugary <ssugary0@gmail.com>
2026-03-20 12:29:28 -0300

    Add(exemplos do uso das funções de DiGraph)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
+#include "include/graph.hpp"
 #include "include/grafos_adjacency.hpp"
 #include "include/grafos_list.hpp"
-#include <cstdlib>
+
+using namespace grp;
 
 int main(int argc, char const *argv[])
 {
     //exemplo do slide
-    Graph g(6);
+    GraphAdj g(6);
      g.add(1, 4);
      g.add(2, 4);
      g.add(2, 3);
@@ -15,13 +17,24 @@ int main(int argc, char const *argv[])
      g.add(6, 4);
      g.print();
     std::cout << "\n\n";
-     GraphList gl(5);
+    GraphList gl(5);
     gl.add(1, 2);
     gl.add(1, 3);
     gl.add(2, 3);
     gl.add(3, 4);
     gl.add(3, 5);
-     gl.print();
-     
+    gl.print();
+    std::cout << "\n\n";
+    DiGraph dg(5);
+    dg.add(1, 2);
+    dg.add(1, 3);
+    dg.add(2, 3);
+    dg.add(3, 4);
+    dg.add(3, 5);
+    dg.print();
+    std::cout << "\n\n";
+    dg.printInc();
+    std::cout << "\n\n";
+    dg.printRStar();
     return 0;
 }
