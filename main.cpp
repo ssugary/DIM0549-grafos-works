0a6097875863f0722865d6b6b6d979b39b9c8796
Leandro Andrade <leandro.anadrade00@gmail.com>
2026-04-08 18:40:39 +0000

    Refatora o main.cpp para utilizar a classe Graph com tipos genéricos e remove código comentado desnecessário

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
-#include "include/grafos_adjacency.hpp"
-#include "include/grafos_list.hpp"
+#include "include/grafos.hpp"
 #include <cstdlib>
 
 int main(int argc, char const *argv[])
 {
-    //exemplo do slide
-    Graph g(6);
+    Graph<int> g(6);
      g.add(1, 4);
      g.add(2, 4);
      g.add(2, 3);
@@ -14,18 +12,18 @@ int main(int argc, char const *argv[])
      g.add(5, 6);
      g.add(6, 4);
      g.print();
-
-     std::cout << "\n";
-     g.dfs(g, 4);
-    std::cout << "\n\n";
-     GraphList gl(5);
-    gl.add(1, 2);
-    gl.add(1, 3);
-    gl.add(2, 3);
-    gl.add(3, 4);
-    gl.add(3, 5);
-    gl.print();
-
+     std::cout << "-------------------\n";
+     g.to_list();
+     g.print();
+    std::cout << "-------------------\n";
+     Graph<char> g2(3);
+        g2.add('A', 'B');
+        g2.add('B', 'C');
+        g2.add('C', 'A');
+        g2.print();
+        g2.to_list();
+    std::cout << "-------------------\n";
+        g2.print();
      
     return 0;
 }
